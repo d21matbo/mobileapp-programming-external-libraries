5e9425f91e857ccab4905a9e4684bdcb78e503f0
Mattias Borgenstierna <d21matbo@student.his.se>
2022-05-16 15:16:10 +0200

    Add code to create a border on CircleImageView during runtime

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CircleImageView circleImageView = findViewById(R.id.profile_view);
+        circleImageView.setBorderWidth(5);
     }
 }
